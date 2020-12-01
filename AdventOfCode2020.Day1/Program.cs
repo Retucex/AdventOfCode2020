92f8a5eb9a867db1c2404f815c4e68adfb1f3d59
Samuel Leclerc <sam.h.leclerc@gmail.com>
2020-12-01 17:25:36 -0500

    Cleaner implementation

--- AdventOfCode2020.Day1/Program.cs
+++ AdventOfCode2020.Day1/Program.cs
@@ -15,35 +15,45 @@ namespace AdventOfCode2020.Day1
 				.Select(int.Parse)
 				.ToList();
 
-			OutputProductOnSum20202Numbers(inputs);
-			OutputProductOnSum20203Numbers(inputs);
+			var results = new List<int>
+			{
+				ProductOnSum20202Numbers(inputs),
+				ProductOnSum20203Numbers(inputs)
+			};
+
+			using var f = new StreamWriter("output.txt");
+			results.ForEach(x => f.WriteLine(x));
 		}
 
-		private static void OutputProductOnSum20202Numbers(List<int> inputs)
+		private static int ProductOnSum20202Numbers(List<int> inputs)
 		{
 			for (int i = 0; i < inputs.Count(); i++)
 			{
 				for (int j = i + 1; j < inputs.Count() - 1; j++)
 				{
 					if (inputs[i] + inputs[j] == 2020)
-						Console.WriteLine(inputs[i] * inputs[j]);
+						return inputs[i] * inputs[j];
 				}
 			}
+
+			throw new Exception("BOOBOO");
 		}
 
-		private static void OutputProductOnSum20203Numbers(List<int> lines)
+		private static int ProductOnSum20203Numbers(List<int> inputs)
 		{
-			for (int i = 0; i < lines.Count; i++)
+			for (int i = 0; i < inputs.Count; i++)
 			{
-				for (int j = i + 1; j < lines.Count - 1; j++)
+				for (int j = i + 1; j < inputs.Count - 1; j++)
 				{
-					for (int k = j + 1; k < lines.Count - 2; k++)
+					for (int k = j + 1; k < inputs.Count - 2; k++)
 					{
-						if (lines[i] + lines[j] + lines[k] == 2020)
-							Console.WriteLine(lines[i] * lines[j] * lines[k]);
+						if (inputs[i] + inputs[j] + inputs[k] == 2020)
+							return inputs[i] * inputs[j] * inputs[k];
 					}
 				}
 			}
+
+			throw new Exception("BOOBOO");
 		}
 	}
 }
