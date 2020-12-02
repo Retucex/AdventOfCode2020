2ef18d8088c3933840ac621f9f6e51757c4c9bc9
Samuel Leclerc <sam.h.leclerc@gmail.com>
2020-12-01 19:02:46 -0500

    Generic numbers to sum

--- AdventOfCode2020.Day1/Program.cs
+++ AdventOfCode2020.Day1/Program.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace AdventOfCode2020.Day1
 {
@@ -17,43 +18,39 @@ namespace AdventOfCode2020.Day1
 
 			var results = new List<int>
 			{
-				ProductOnSum20202Numbers(inputs),
-				ProductOnSum20203Numbers(inputs)
+				GetNumbersWithSumX(inputs, 2020, 2).Aggregate((a, x) => a * x),
+				GetNumbersWithSumX(inputs, 2020, 3).Aggregate((a, x) => a * x),
 			};
 
 			using var f = new StreamWriter("output.txt");
 			results.ForEach(x => f.WriteLine(x));
 		}
 
-		private static int ProductOnSum20202Numbers(List<int> inputs)
+		private static List<int> GetNumbersWithSumX(List<int> inputs, int sum, int quantityToSum)
 		{
-			for (int i = 0; i < inputs.Count(); i++)
-			{
-				for (int j = i + 1; j < inputs.Count() - 1; j++)
-				{
-					if (inputs[i] + inputs[j] == 2020)
-						return inputs[i] * inputs[j];
-				}
-			}
-
-			throw new Exception("BOOBOO");
+			var partial = new int[quantityToSum];
+			return GetNumbersWithSumX(inputs, partial, sum, quantityToSum - 1, quantityToSum - 1, 0);
 		}
 
-		private static int ProductOnSum20203Numbers(List<int> inputs)
+		private static List<int> GetNumbersWithSumX(List<int> inputs, int[] partial, int sum, int initial, int remaining, int start)
 		{
-			for (int i = 0; i < inputs.Count; i++)
+			for (int i = start; i < inputs.Count; i++)
 			{
-				for (int j = i + 1; j < inputs.Count - 1; j++)
+				partial[remaining] = inputs[i];
+				if (remaining != 0)
 				{
-					for (int k = j + 1; k < inputs.Count - 2; k++)
-					{
-						if (inputs[i] + inputs[j] + inputs[k] == 2020)
-							return inputs[i] * inputs[j] * inputs[k];
-					}
+					var result = GetNumbersWithSumX(inputs, partial, sum, initial, remaining - 1, i + 1);
+					if (result != null)
+						return result;
+				}
+				else
+				{
+					if(partial.Sum() == sum)
+						return partial.ToList();
 				}
 			}
 
-			throw new Exception("BOOBOO");
+			return null;
 		}
 	}
 }
